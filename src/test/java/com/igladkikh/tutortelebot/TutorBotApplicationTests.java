2d2c00a5ac9c893cb488dcb60cec618b090c68a6
Igor Gladkikh <kla2005@yandex.ru>
2024-12-02 13:38:35 +0300

    Initial commit

---
+++ src/test/java/com/igladkikh/tutortelebot/TutorBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.igladkikh.tutortelebot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutorBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
